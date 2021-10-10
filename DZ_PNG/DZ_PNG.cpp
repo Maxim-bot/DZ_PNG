99d639e975a6e577655d57f03f3a46a7866fe03a
Макс E <max0681832239@hotmail.com>
2021-10-10 12:18:56 +0300

    Добавьте файлы проекта.

---
+++ DZ_PNG/DZ_PNG.cpp
@@ -0,0 +1,38 @@
+﻿//DZ_#1
+
+#include<vector>
+#include<iostream>
+#include<time.h>
+
+using namespace std;
+
+int main()
+{
+	const int n = 3;
+	vector<int>arr0{ 0,1,2 };
+	cout << arr0[2] << endl;
+	arr0.resize(10);
+	cout << arr0[9] << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		int n;
+		n = rand() % 10;
+		arr0[i] = n;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << " " << arr0[i];
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		int n = arr0[i];
+		n = n * n;
+		arr0[i] = n;
+		cout << " " << arr0[i];
+	}
+}
